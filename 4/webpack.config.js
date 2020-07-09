b0a6bd739b46cd7a99438353f92c2716af5d160b
inmax <inmaculadaminarroruiz@gmail.com>
2020-07-09 17:59:40 +0200

    update config webpack

--- 4/webpack.config.js
+++ 4/webpack.config.js
@@ -1,24 +1,37 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
 module.exports = {
-    mode: 'development',
-    entry: './src/app.js',
-    output: {
-        filename: 'bundle.js'
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /(node_modules)/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: [
-                            '@babel/preset-env',
-                            '@babel/preset-es2015'
-                        ],
-                    }
-                }
-            }
-        ]
-    }
+  mode: "development",
+  entry: "./src/app.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devServer: {
+   contentBase: './dist',
+ },
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+      { enforce: "pre", test: /\.js$/, loader: "source-map-loader" },
+    ],
+  },
+  plugins: [
+    new CleanWebpackPlugin({ cleanStaleWebpackAssets: false }),
+    new HtmlWebpackPlugin({
+      title: "Development",
+      template: path.resolve("//" + __dirname, "web.html"),
+    }),
+  ]
 };
